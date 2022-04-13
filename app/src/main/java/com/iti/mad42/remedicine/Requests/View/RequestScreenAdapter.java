37e1b079cb3221ae51745710687337f49ba3bd93
SandraGeorge19 <georgesandra592@gmail.com>
2022-04-13 14:48:03 +0200

    finishing requests screen

--- app/src/main/java/com/iti/mad42/remedicine/Requests/View/RequestScreenAdapter.java
+++ app/src/main/java/com/iti/mad42/remedicine/Requests/View/RequestScreenAdapter.java
@@ -1,37 +1,75 @@
 package com.iti.mad42.remedicine.Requests.View;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.iti.mad42.remedicine.Model.Medication;
+import com.iti.mad42.remedicine.R;
+
+import java.util.List;
+
 public class RequestScreenAdapter extends RecyclerView.Adapter<RequestScreenAdapter.ViewHolder> {
 
-    public RequestScreenAdapter(){
+    private final Context context;
+    private List<Medication> myMeds;
 
+    public RequestScreenAdapter(Context _context, List<Medication> myMeds){
+        this.context = _context;
+        this.myMeds = myMeds;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public TextView userName;
+        public Button acceptBtn, rejectBtn;
+        public View layout;
+
+        public ViewHolder(View layout) {
+            super(layout);
+            this.layout = layout;
+            userName = layout.findViewById(R.id.requestUserName);
+            acceptBtn = layout.findViewById(R.id.acceptBtn);
+            rejectBtn = layout.findViewById(R.id.rejectBtn);
         }
     }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View layout = inflater.inflate(R.layout.requests_custom_cell,parent, false);
+        ViewHolder requestViewHolder = new ViewHolder(layout);
+        return requestViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.userName.setText(myMeds.get(position).getMedName());
+        holder.acceptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Accept Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.rejectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Reject Button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return myMeds.size();
     }
 
 
--- app/src/main/java/com/iti/mad42/remedicine/Requests/View/RequestsViewActivity.java
+++ app/src/main/java/com/iti/mad42/remedicine/Requests/View/RequestsViewActivity.java
@@ -1,16 +1,41 @@
 package com.iti.mad42.remedicine.Requests.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.iti.mad42.remedicine.Model.Medication;
 import com.iti.mad42.remedicine.R;
 
+import java.util.ArrayList;
+
 public class RequestsViewActivity extends AppCompatActivity {
+
+    RecyclerView requestsRecycler;
+    RequestScreenAdapter requestAdapter;
+    ArrayList<Medication> myMeds = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requests_view);
+        myMeds.add(new Medication("Panadol", "500","g", "After Eating", "15"));
+        myMeds.add(new Medication("Panadol", "500","g", "After Eating", "15"));
+        myMeds.add(new Medication("Panadol", "500","g", "After Eating", "15"));
+
+        requestsRecycler = findViewById(R.id.requestsRecycler);
+        requestsRecycler.setHasFixedSize(true);
+        requestAdapter = new RequestScreenAdapter(RequestsViewActivity.this, myMeds);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+
+        requestsRecycler.setLayoutManager(layoutManager);
+
+        requestsRecycler.setAdapter(requestAdapter);
+
+        requestAdapter.notifyDataSetChanged();
     }
 }
